5bb0b6c3eec5e25885325c89c5306944db2ddcc5
MartinG94 <guillenmartin94@gmail.com>
2020-05-21 13:15:18 -0300

    Corrección en registroDeRutas()

--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
 
 const registroDeRutas = (req, res, next) => {
-    const frase = 'El usuario ingresó a la ruta: '
-    fs.appendFileSync('./logs/userLogs.txt', frase + req.url);
+    fs.appendFileSync('userLogs.txt', `El usuario ingresó a la ruta: ${req.url}\n`);
     next();
 };
 
