14aa9209c367f120580baefacb73d375732e19b1
MartinG94 <guillenmartin94@gmail.com>
2020-05-21 13:02:59 -0300

    App ahora utiliza el middleware userLogs

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ app.set('views', './src/views'); // Seteo de la ubicación de la carpeta "views"
 
 // ************ WRITE YOUR CODE FROM HERE ************
 // ************ Route System require and use() ************
+const userLogs = require('./middlewares/userLogs');
+app.use(userLogs);
 const mainRouter = require('./routes/main');
 app.use('/', mainRouter);
 
